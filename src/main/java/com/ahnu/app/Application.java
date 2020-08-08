4a1f135b47ed8772c807ea5b30c7785262e1ed0e
SSW <DamonCheng@ssw.com.au>
2020-08-08 23:32:48 +0800

    启动类

---
+++ src/main/java/com/ahnu/app/Application.java
@@ -0,0 +1,57 @@
+package com.ahnu.app;
+
+import com.ahnu.app.service.Task;
+
+import java.io.IOException;
+
+import static com.ahnu.app.service.Task.stop;
+
+/**
+ * 启动方法
+ *
+ * @author DamonCheng@ssw.com.au
+ * @date 8/4/2020 3:42 PM
+ */
+public class Application {
+    /**
+     * 每天启动的小时数
+     */
+    private final static int HOUR = 6;
+    /**
+     * 每天启动的分钟数
+     */
+    private final static int MINUTE = 50;
+
+    /**
+     * 每天启动的秒数
+     */
+    private final static int SECOND = 0;
+    /**
+     * 时间间隔(一天)
+     */
+    private final static long PERIOD_DAY = 24L * 60L * 60L * 1000L;
+
+    /**
+     * 心跳包 2小时一跳
+     */
+    final static int HEART_BEAT = 1000 * 60 * 60 * 2;
+
+    /**
+     * 程序入口
+     *
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------");
+        System.out.println("----------------start-----------------");
+        System.out.println("--------------------------------------");
+        Task task = new Task();
+        while (true) {
+            task.start();
+            stop(HEART_BEAT);
+        }
+    }
+
+
+}
