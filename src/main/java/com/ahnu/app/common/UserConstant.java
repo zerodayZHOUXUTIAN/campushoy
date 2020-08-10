6282b678ef16f03930429ba2b87cc293577e56a5
SSW <DamonCheng@ssw.com.au>
2020-08-10 09:14:24 +0800

    用户cookie信息，学校域名

---
+++ src/main/java/com/ahnu/app/common/UserConstant.java
@@ -0,0 +1,28 @@
+package com.ahnu.app.common;
+
+/**
+ * 用户需要填的信息
+ *
+ * @author DamonCheng@ssw.com.au
+ * @date 8/6/2020 11:53 AM
+ */
+public interface UserConstant {
+    /**
+     * 安徽师范大学的域名
+     * 请求的学校域名；每个学校都不相同
+     */
+    String DOMAIN = "https://ahnu.cpdaily.com";
+
+    /**
+     * 不退出就固定不变的值
+     */
+    String CPDAILY_EXTENSION = "";
+
+
+    /**
+     * 用户的MOD_AUTH_CAS信息
+     */
+    String COOKIE = "";
+
+
+}
