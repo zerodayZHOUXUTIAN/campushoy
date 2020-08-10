477475fe07bfdee47285e268be5f0ed470d7187c
SSW <DamonCheng@ssw.com.au>
2020-08-10 09:12:40 +0800

    邮件标题常量

---
+++ src/main/java/com/ahnu/app/common/EmailConstant.java
@@ -0,0 +1,12 @@
+package com.ahnu.app.common;
+
+/**
+ * @author DamonCheng@ssw.com.au
+ * @date 8/4/2020 4:43 PM
+ */
+public interface EmailConstant {
+
+    String SUCCESS = "疫情信息提交成功";
+    String FAIL = "疫情信息提交失败";
+
+}
